0039018d808a3d35296450a6fafcf5f7c0437b19
kungelee <i@jefflee.me>
2020-12-05 15:04:34 +0800

    prime number

--- foo.c
+++ foo.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
-int main(void)
+int isprime(int n)
 {
-	
-	printf("hello,world");	
-	return 0;
+	int i;
+	for (i = 2; i < n; i++) {
+		if (n % i == 0)
+			break;
+	}
+
+	return i == n;
 }
 
+int main(void)
+{
+	int x;
+	for (x = 1; x <= 1000; x++) {
+		static int count = 0;
+		if (isprime(x)) {
+			if (count % 10 == 0 && count != 0)
+				printf("\n");
+			printf("%4d", x);
+			count++;
+		}
+	}
+	return 0;
+}
 
