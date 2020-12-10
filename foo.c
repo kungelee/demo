75fafdd6011f137688ffd441281e3356e90e84f7
kungelee <i@jefflee.me>
2020-12-10 23:23:50 +0800

    struct

--- foo.c
+++ foo.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	int x = 0;
-	if (scanf("%d", &x) != 1 || (x <= -5) || (x >= 8)) {
-		printf("scanf() error!\n");
-		exit(1);
-	}
-	int y = 0;
-	int codition = 0;
-	codition = (x > -5 && x < 0) ? -1 : 1;
+typedef struct _cat {
+	char name[10];
+	char color[10];
+	void (*say)(void);  //叫
+	void (*jump)(void); //跳
+} CAT;
 
-	switch (codition) {
-	case -1:
-		y  = x - 1;
-		break;
-	case 0:
-		y = 0;
-		break;
-	case 1:
-		y = x + 1;
-	default:
-		break;
 
-	}
+void say(void)
+{
+	printf("Miao\n");
+}
 
-	printf("y = %d\n", y);
+void jump(void)
+{
+	printf("jump\n");
+}
+
+
+int main()
+{
+
+	//创建5只独
+	//初始化第一只
+	CAT arr[5] = {
+		{
+		name: "小花",
+		color: "白色",
+		say:say,
+		jump:jump
+		},
+	};
+
+	//打印技能
+	printf("name = %s, color = %s\n", arr[0].name, arr[0].color);
+	arr[0].say();
+	arr[0].jump();
 
 	exit(0);
 }
