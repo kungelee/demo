590a352e9ca36000b9bf32bdd93a3b54aa083c12
kungelee <i@jefflee.me>
2020-12-13 16:53:47 +0800

    ? operator

--- foo.c
+++ foo.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
 
 int main()
 {
 
-  printf("hello, world\n");
-  printf("Welcome to Manjaro wrold\n");
-  getchar();
+  char *ptr = "我是一条狗啊";
+  srand((unsigned) time(NULL));
+  bool condition = false;
+  
+  ptr  = (condition = rand() % 2) ? "我是旺财" : "我是铁蛋";
+  fprintf(stdout, "%d: ", condition);
+  puts(ptr);
+  
   exit(0);
 }
 
