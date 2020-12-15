2a05e57317a2be026cc7ec7001c267011898574e
kun.ge <kunge.lee@gmail.com>
2020-12-15 19:40:08 +0800

    printer  sort

--- foo.c
+++ foo.c
@@ -1,44 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct _cat {
-	char name[10];
-	char color[10];
-	void (*say)(void);  //叫
-	void (*jump)(void); //跳
-} CAT;
-
-
-void say(void)
+//3)return the max number address
+int *max(int arr[], int size)
 {
-	printf("Miao\n");
-}
-
-void jump(void)
-{
-	printf("jump\n");
+	int the_max = arr[0];
+	int index = 0;
+	int i = 0;
+	for (i = 1; i < size; i++) {
+		if (the_max < arr[i]) {
+			the_max = arr[i];
+			index = i;
+		}
+	}
+	return &arr[index];
+	
 }
 
 
-int main()
+//3)return the min number address
+int *min(int arr[], int size)
 {
+	int the_min = arr[0];
+	int index = 0;
+	int i = 0;
+	for (i = 1; i < size; i++) {
+		if (the_min > arr[i]) {
+			the_min = arr[i];
+			index = i;
+		}
+	}
+	return &arr[index];
+	
+}
 
-	//创建5只独
-	//初始化第一只
-	CAT arr[5] = {
-		{
-		name: "小花",
-		color: "白色",
-		say:say,
-		jump:jump
-		},
-	};
 
-	printf("Skill output:\n");
-	//打印技能
-	printf("name = %s, color = %s\n", arr[0].name, arr[0].color);
-	arr[0].say();
-	arr[0].jump();
 
-	exit(0);
+int main(void)
+{
+	//1)define ten integer numbers 
+	int arr[10] = {0};
+	int i;
+	
+	//2)input ten numbers
+	printf("Enter ten numbers:\n");
+	for (i = 0; i < 10; i++)
+		scanf("%d", &arr[i]);
+	
+	//4)use pointer record the max  and min address
+	int *max_ptr = max(arr, 10);
+	int *min_ptr = min(arr, 10);
+	
+	//5)print original array
+	printf("Before swap:\n");
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	
+	//swap
+	int temp = *max_ptr;
+	*max_ptr = *min_ptr;
+	*min_ptr = temp;
+	
+	
+	//5) print swap  array
+	printf("\nAfter swap:\n");
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	
+	return 0;
 }
