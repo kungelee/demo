7bdd55d3d2d162b6c449119d3c831c5d2b1bf75d
kungelee <i@jefflee.me>
2021-02-03 00:58:34 +0800

    1:2:3

--- foo.c
+++ foo.c
@@ -1,76 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <stdbool.h>
-
-typedef struct node {
-	int data;
-	struct node *next ;
-} Node;
-
-//init list  and assign value
-bool init_list(Node** head, int const *data, size_t len)
+/*
+将1,2....,9共9个数分成3组，分别组成3个三位数，
+且使这3个三位数构成1 : 2 : 3的比例，
+试求出所有满足条件的3个三位数。
+2021年2月3日00:57:59
+*/
+#include <stdio.h>
+int main()
 {
-	Node* current;
-	Node* tail;
-	current = tail = NULL;
-	for (size_t i = 0; i < len; i++) {
-		current = (Node *) malloc(sizeof(Node));
-		if (NULL == current)
-			return false;
-		current->next = NULL;
-		current->data = data[i];
-		if (NULL == *head) {
-			*head = current;
-			tail = current;
-		} else {
-			tail->next = current;
-			tail = current;
-		}
-	}
-	return true;
-}
-
-//print list
-void print_list(Node const *head)
-{
-	printf("%s", "START");
-	while (head != NULL) {
-		printf(" -> [%d]", head->data);
-		head = head->next;
-	}
-	printf("%s\n", "-> END");
-}
-
-//not find return 0
-size_t find(Node const *head, int key)
-{
-	size_t index = 1;
-	while ((head != NULL) && (head->data != key)) {
-		index++;
-		head = head->next;
-	}
-	return (head == NULL) ? 0 : index;
-}
-int main(void)
-{
-	Node* head;
-	int key;
-	int data[]= {10,7,12,66,99};
-
-	head = NULL;
-	if (! init_list(&head, data, sizeof(data)/sizeof(data[0])))
-		return 1;
-
-	print_list(head);
-	printf("Enter search key: ");
-	if (scanf("%d", &key) != 1)
-		return 1;
-
-	size_t index = find(head,key);
-	if(0 == index)
-		printf("Not Found\n");
-	else
-		printf("The index is: %u\n", index);
-
-	return 0;
+    for(int n = 100; n < 333; n++)
+    {
+		/* 生成满足条件的三个数 */
+        int a = n;
+		int b = n * 2;
+		int c = n * 3;
+		
+		/* 保存待验证的三个数 */
+		const int a1 = a;
+		const int b1 = b;
+		const int c1 = c;
+		/* 统计余数出现的次数 */
+		int cnt_number[10] = {0}; 
+        while(a)
+        {
+			cnt_number[a % 10]++;
+			cnt_number[b % 10]++;
+			cnt_number[c % 10]++;
+			
+			/* 数字中含有零，条件不满足 */
+			if (cnt_number[0] != 0)
+				break;
+			
+        	a /= 10;
+            b /= 10;
+            c /= 10;
+	
+        }
+		/* 有零值,直接跳过验证 */
+		if (cnt_number[0] != 0) 
+			continue;
+        
+		int len = 0;
+        for(int i = 1; i < 10; i++)
+        {	
+			/* 只统计出现过一次的情况 */
+            if(1 != cnt_number[i]) 
+				break;
+            len++;
+        }
+        if(9 == len)
+        {
+            printf("%d, %d, %d\n", a1, b1, c1);
+        }
+    }
+    return 0;
 }
